c28b2da1b6776c503a0af76766756cbf8e7fb239
Mkamboykos <mmeri032@fiu.edu>
2021-05-31 04:19:36 -0400

    Added serviceworker files and form to home.js

--- src/Login_Page/Home.js
+++ src/Login_Page/Home.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import {Form} from 'react-bootstrap'
+
 /*   Home.js is rendered in App.js    */
 
 function Home() {
     return(
         <div>
             <h1>Time Waiter</h1>
+
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="email" placeholder="Username" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+            </Form>
 
             <button className="signUp_button_home"><Link to="/SignUp" className="link">Sign Up</Link></button>
             <button className="login_button"><Link to="/Dashboard" className="link">Login</Link></button>
