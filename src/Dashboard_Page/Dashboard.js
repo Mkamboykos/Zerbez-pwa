fc9c5fdc0b625742bfa9f6ac36a5b2f9532626fc
Mkamboykos <mmeri032@fiu.edu>
2021-06-25 14:52:39 -0400

    Added Settings icon and transition animation with Sping

--- src/Dashboard_Page/Dashboard.js
+++ src/Dashboard_Page/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { useSpring } from "react-spring";
-import { animated } from "react-spring";
-
+import { useSpring, animated } from "react-spring";
+import { GoThreeBars } from "react-icons/go"
+import { IoClose } from "react-icons/io"
 
 /*   Dashboard.js is rendered in App.js    */
 
@@ -9,24 +9,22 @@ function Dashboard() {
 
     const [rightMenuVisible, setRightMenuVisible] = useState(false);
 
-  
-
     const MenuRight = ({ style }) => (
         <animated.div className="menu menu--right" style={style}>
             <nav>
                 <ul className="menu-list menu-list--right">
-                <li className="menu-list-item menu-list-item--right">
-                    <a href="/">Home</a>
-                </li>
-                <li className="menu-list-item menu-list-item--right">
-                    <a href="/">About</a>
-                </li>
-                <li className="menu-list-item menu-list-item--right">
-                    <a href="/">Work</a>
-                </li>
-                <li className="menu-list-item menu-list-item--right">
-                    <a href="/">Contact</a>
-                </li>
+                    <li className="menu-list-item menu-list-item--right">
+                        <a href="/">Home</a>
+                    </li>
+                    <li className="menu-list-item menu-list-item--right">
+                        <a href="/">About</a>
+                    </li>
+                    <li className="menu-list-item menu-list-item--right">
+                        <a href="/">Work</a>
+                    </li>
+                    <li className="menu-list-item menu-list-item--right">
+                        <a href="/">Contact</a>
+                    </li>
                 </ul>
             </nav>
         </animated.div>
@@ -34,14 +32,14 @@ function Dashboard() {
 
     const rightMenuAnimation = useSpring({
         opacity: rightMenuVisible ? 1 : 0,
-        transform: rightMenuVisible ? `translateX(0)` : `translateX(100%)`
+        transform: rightMenuVisible ? `flash(0)` : `flash(100%)`
     });
         
     return (
         <div className="Dashboard">
-            <button className="menu-button" onClick={() => setRightMenuVisible(!rightMenuVisible)}>
-                {rightMenuVisible ? "Close" : "Side Menu"}
-            </button>
+            <GoThreeBars className="menu-button" onClick={() => setRightMenuVisible(!rightMenuVisible)}>
+                {rightMenuVisible ? <GoThreeBars/> : <GoThreeBars/>}
+            </GoThreeBars>
     
             <MenuRight style={rightMenuAnimation} />
         </div> 
