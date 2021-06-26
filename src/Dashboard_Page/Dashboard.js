47df8872e00c16f86d71071e0ff2227c3b797e74
Mkamboykos <mmeri032@fiu.edu>
2021-06-26 15:46:37 -0400

    debugging dashboard buttons

--- src/Dashboard_Page/Dashboard.js
+++ src/Dashboard_Page/Dashboard.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import { GoThreeBars } from "react-icons/go"
-import { IoClose } from "react-icons/io"
+import {Link} from 'react-router-dom'
+
 
 /*   Dashboard.js is rendered in App.js    */
 
@@ -14,16 +15,22 @@ function Dashboard() {
             <nav>
                 <ul className="menu-list menu-list--right">
                     <li className="menu-list-item menu-list-item--right">
-                        <a href="/">Home</a>
+                        <a href="/Dashboard">Home</a>
                     </li>
                     <li className="menu-list-item menu-list-item--right">
-                        <a href="/">About</a>
+                        <a href="/Account">Account</a>
                     </li>
                     <li className="menu-list-item menu-list-item--right">
-                        <a href="/">Work</a>
+                        <a href="/Analytics">Analytics</a>
                     </li>
                     <li className="menu-list-item menu-list-item--right">
-                        <a href="/">Contact</a>
+                        <a href="/Notifications">Notifications</a>
+                    </li>
+                    <li className="menu-list-item menu-list-item--right">
+                        <a href="/Help">Help</a>
+                    </li>
+                    <li className="menu-list-item menu-list-item--right">
+                        <a href="/About">About</a>
                     </li>
                 </ul>
             </nav>
@@ -38,13 +45,22 @@ function Dashboard() {
     return (
         <div className="Dashboard">
             <GoThreeBars className="menu-button" onClick={() => setRightMenuVisible(!rightMenuVisible)}>
-                {rightMenuVisible ? <GoThreeBars/> : <GoThreeBars/>}
+            {rightMenuVisible ? <GoThreeBars/> : <GoThreeBars/>}
             </GoThreeBars>
-    
+          
             <MenuRight style={rightMenuAnimation} />
-        </div> 
-    );
+            <button className="Dashboard_button_FloorPlan"><Link to="/FloorPlan" className="link"><b>FLOOR PLAN</b></Link></button>
+            <button className="Dashboard_button_NewReservation"><Link to="/NewReservation" className="link"><b>New Reservation</b></Link></button>
+            <button className="Dashboard_button_Reserations"><Link to="/Reservations" className="link"><b>Reservations</b></Link></button>
+            <button className="Dashboard_button_ShiftSchedule"><Link to="/ShiftSchedule" className="link"><b>SHIFT SCHEDULE</b></Link></button>
+            <button className="Dashboard_button_AssignTables"><Link to="/ShiftSchedule" className="link"><b>ASSIGN TABLES</b></Link></button>
+            <button className="Dashboard_button_TimePerCover"><Link to="/TimePerCover" className="link"><b>TIME PER COVER</b></Link></button>
+            <button className="Dashboard_button_EndShift"><Link to="/" className="link"><b>END OF SHIFT</b></Link></button>
       
-    }
+      </div>
+    );
+
+      
+}
 
 export default Dashboard;
