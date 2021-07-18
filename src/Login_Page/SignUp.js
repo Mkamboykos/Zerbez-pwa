dbc924302f7fb9326bae1e96d5967d4818fc1bd7
Manuel Kamboykos <60014618+Mkamboykos@users.noreply.github.com>
2021-07-18 03:09:44 -0400

    setup container for sign up page

--- src/Login_Page/SignUp.js
+++ src/Login_Page/SignUp.js
@@ -6,12 +6,17 @@ import {IoChevronBack} from 'react-icons/io5'
 /*   SignUp.js is rendered in App.js    */
 
 function SignUp() {
-    return(
-        <div>
-            <h1>Sign Up</h1>
-            <Link to="/" className="link"><IoChevronBack className="Back_button_SignUp"/></Link>
-            <button className="signUp_button_two"><Link to="/DashBoard" className="link">Sign Up</Link></button>
 
+    
+    return(
+        <div className="SignUp_Page_Container">
+            <Link to="/" className="link"><IoChevronBack className="Back_button_SignUp"/></Link>
+            <h1 className="SignUp_Page_Title">Sign Up</h1>
+            
+
+            
+            <button className="signUp_button_two"><Link to="/DashBoard" className="link">SIGN UP</Link></button>
+    
         </div>
     )
 }
