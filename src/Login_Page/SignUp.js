d52a77079b245857602b95aad0cafbd4bd359688
Manuel Kamboykos <60014618+Mkamboykos@users.noreply.github.com>
2021-07-21 00:25:07 -0400

    Set up material-ui textfields

--- src/Login_Page/SignUp.js
+++ src/Login_Page/SignUp.js
@@ -1,77 +1,98 @@
-import React from 'react'
+import React, { useState, Col, makeStyles } from 'react'
 import {Link} from 'react-router-dom'
 import {Form} from 'react-bootstrap'
+import {TextField} from '@material-ui/core'
+
 import {IoChevronBack} from 'react-icons/io5'
 
 /*   SignUp.js is rendered in App.js    */
 
 function SignUp() {
 
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+
     return(
         <div className="SignUp_Page_Container">
             <Link to="/" className="link"><IoChevronBack className="Back_button_SignUp"/></Link>
             <h1 className="SignUp_Page_Title">Sign Up</h1>
             
-            <Form>
-                <Form.Group className="" controlId="formBasicEmail">
-                    <Form.Control 
-                        type="email" 
-                        placeholder="First Name" 
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group controlId="validationCustom01" as={Col} md="4">
+                    <TextField
+                        required
+                        className="signUp_TextField"
+                        variant ="filled"
+                        label="First Name"
+                        type="text"
                     />
                 </Form.Group>
 
-                <Form.Group className="" controlId="formBasicEmail">
-                    <Form.Control 
-                        type="email" 
-                        placeholder="Last Name" 
-                    />
+                <Form.Group controlId="validationCustom02" as={Col} md="4">
+                    <Form.Control required type="text" placeholder="Last Name" />
                 </Form.Group>
 
-                <Form.Group className="" controlId="formBasicEmail">
+                <Form.Group className="" htmlFor="inputPassword5" controlId="formBasicEmail">
                     <Form.Control 
-                        type="email" 
+                        type="password" 
+                        id="inputPassword5"
+                        aria-describedby="passwordHelpBlock"
                         placeholder="Password" 
                     />
+                    <Form.Text id="passwordHelpBlock" muted>
+                        Your password must be 8-20 characters long, contain letters and numbers, and
+                        must not contain spaces, special characters, or emoji.
+                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="" controlId="formBasicPassword">
                     <Form.Control 
-                        type="password" 
+                        type="confirmPassword" 
                         placeholder="Confirm Password" 
                     />
                 </Form.Group>
 
                 <Form.Group className="" controlId="formBasicPassword">
                     <Form.Control 
-                        type="password" 
+                        type="restaurantName" 
                         placeholder="Restaurant Name" 
                     />
                 </Form.Group>
 
                 <Form.Group className="" controlId="formBasicPassword">
                     <Form.Control 
-                        type="password" 
+                        type="restaurantAddress" 
                         placeholder="Restaurant Address" 
                     />
                 </Form.Group>
 
                 <Form.Group className="" controlId="formBasicPassword">
                     <Form.Control 
-                        type="password" 
+                        type="city" 
                         placeholder="City" 
                     />
                 </Form.Group>
 
                 <Form.Group className="" controlId="formBasicPassword">
                     <Form.Control 
-                        type="password" 
+                        type="state" 
                         placeholder="State" 
                     />
                 </Form.Group>
 
                 <Form.Group className="" controlId="formBasicPassword">
                     <Form.Control 
-                        type="password" 
+                        type="zipCode" 
                         placeholder="ZIP Code" 
                     />
                 </Form.Group>
@@ -82,9 +103,8 @@ function SignUp() {
                         label="I affirm to be currently working as a host/hostess at this location." 
                     />
                 </Form.Group>
-
             </Form>
-            
+
             <button className="signUp_button_two"><Link to="/DashBoard" className="link">SIGN UP</Link></button>
         </div>
     )
