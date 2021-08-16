908e0a06883c1036a83b952b017a35f0496a330d
Mkamboykos <mmeri032@fiu.edu>
2021-08-15 23:03:12 -0400

    Added back-end server file, prior to dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
     <div className="App">
 
       {/* The Router is use to create different links for each class */}
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Home}/>
       <Route exact path="/SignUp" component={SignUp}/>
       <Route exact path="/Dashboard" component={Dashboard}/>
       <Route exact path="/ForgotPassword" component={ForgotPassword}/>
